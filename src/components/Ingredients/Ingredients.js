669c1832bd41a39eacfa9e2e3c7762c3a19ce832
naracodes <naralee0124@gmail.com>
2021-04-02 00:32:30 -0400

    add hooks to filter

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,37 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import IngredientForm from './IngredientForm';
-import IngredientList from './IngredientList';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 const Ingredients = () => {
-  const [ userIngredients, setUserIngredients ] = useState([]);
+  const [userIngredients, setUserIngredients] = useState([]);
 
-  const addIngredientHandler = ingredient => {
-    fetch('https://react-hooks-dc979-default-rtdb.firebaseio.com/ingredients.json', {
-      method: 'POST',
-      body: JSON.stringify(ingredient),
-      headers: { 'Content-Type': 'application/json' }
-    }).then(response => {
-      return response.json();
-    }).then(responseData => {
-      setUserIngredients(prevIngredients =>
-         [...prevIngredients, { id: responseData.name, ...ingredient}]
-        //  [...prevIngredients, { id: Math.random().toString(), ...ingredient}]
-      )
-    });
-  }
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-dc979-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []); // componentDidMount
+
+  const filteredIngredientsHandler = (filteredIngredients) => {
+    setUserIngredients(filteredIngredients);
+  };
+
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      "https://react-hooks-dc979-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients(
+          (prevIngredients) => [
+            ...prevIngredients,
+            { id: responseData.name, ...ingredient },
+          ]
+          //  [...prevIngredients, { id: Math.random().toString(), ...ingredient}]
+        );
+      });
+  };
 
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
